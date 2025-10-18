79634755bbb069ffa3b2f1f39d67dc114bcf70de
SHAN <ShanKonduru@gmail.com>
2025-10-18 08:02:40 -0600

    working dashboard

--- main_local.py
+++ main_local.py
@@ -218,4 +218,4 @@ async def get_metrics():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run(app, host="127.0.0.1", port=8001)
